d7c0dd349a791a634c63bf0bc1a7ae251eb5ebee
Hector Castro <raidrex@hotmail.com>
2024-09-16 20:03:28 -0600

    mensaje de contraseña segura en vez de un alert

--- main.js
+++ main.js
@@ -2,56 +2,61 @@ let cantidad = document.getElementById('cantidad');
 let boton = document.getElementById('generar');
 let constraseña = document.getElementById('contrasena');
 let botonLimpiar = document.getElementById('limpiar');
+let mensaje = document.getElementById('mensaje');
 
 const cadenaDeCaracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
 
 
 function generar() {
-     
-    let numeroSolicitado = parseInt (cantidad.value);
-    
+
+    let numeroSolicitado = parseInt(cantidad.value);
+
     if (numeroSolicitado < 8) {
         alert('La cantidad de caracteres deve ser mayor a 8');
+        return
     }
 
     let contraseña = '';
 
     for (let i = 0; i < numeroSolicitado; i++) {
-        let caracterAleatorio = cadenaDeCaracteres[Math.floor(Math.random()* cadenaDeCaracteres.length)];
-        
+        let caracterAleatorio = cadenaDeCaracteres[Math.floor(Math.random() * cadenaDeCaracteres.length)];
+
         contraseña += caracterAleatorio;
     }
 
-    if(tieneMayusculas(contraseña) && tieneEspeciales(contraseña) && tieneNumeros(contraseña)){ //condicioinales  para verificar si tiene al menos un caracter de cada tipo alert contraseña segura
-         constraseña.value = contraseña
-         alert('contraseña segura');
-    }else {
-         constraseña.value = contraseña
-         alert('contraseña débil');
+    constraseña.value = contraseña
+
+    if (tieneMayusculas(contraseña) && tieneEspeciales(contraseña) && tieneNumeros(contraseña)) { //condicioinales  para verificar si tiene al menos un caracter de cada tipo alert contraseña segura
+        mensaje.textContent = 'Contrasena segura';
+        mensaje.style.color ='green';
+
+    } else {
+        mensaje.textContent = 'Contrasena no segura';
+        mensaje.style.color = 'red';
     }
 }
 
 
-function tieneMayusculas (contraseña){
+function tieneMayusculas(contraseña) {
     return /[A-Z]/.test(contraseña); //   /[A-Z]/.test(contaseña) revisa entre todo el abecedario en mayusculas
 
 }
 
-function tieneEspeciales(contraseña){
+function tieneEspeciales(contraseña) {
     return /[!@#$%^&*()]/.test(contraseña); //   /[!@#$%^&*()]/.test(contraseña) revisa si hay al menos un caracter especial esta en la contraseña
 
 }
 
-function tieneNumeros(contraseña){
+function tieneNumeros(contraseña) {
 
     return /[0-9]/.test(contraseña); //   /[0-9]/.test(contraseña) revisa si hay al menos un caracter numerico esta en la contraseña
 
 }
 
-function limpiar(){ //borra los valores de contraseña y cantidad
+function limpiar() { //borra los valores de contraseña y cantidad
     contrasena.value = '';
     cantidad.value = '';
-    
+
 }
 
 
